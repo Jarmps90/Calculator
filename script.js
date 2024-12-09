09d203ea910d13e11db9e45af3070d32b1172845
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-12-09 08:06:00 +0200

    added functionality to equal button added calculation logic

--- script.js
+++ script.js
@@ -2,38 +2,69 @@ const numbDispaly = document.querySelector('#display');
 let currentNumber = '';
 let previousNumber = null;
 let value = null;
+let operator = null;
 
 function getCalcBtns() {
-const operantsBtns = operants.querySelectorAll('button');
+const operantsBtns = document.querySelectorAll('#operants button');
 
 operantsBtns.forEach((button) => {
     button.addEventListener('click', () => {
     console.log(button.id)
-      currentNumber += button.textContent
-      updateDisplay(currentNumber)
+      currentNumber += button.id
+      numbDispaly.textContent = currentNumber;
     });
 });
 
 };
 
 function getOperatorsBtns(){
-const operatrosBtns = operators.querySelectorAll('button');
+const operatrosBtns = document.querySelectorAll('#operators button');
 
     operatrosBtns.forEach((button) => {
         button.addEventListener('click', () => {
-            numbDispaly.textContent = '0';
+            operator = button.id;
             previousNumber = parseFloat(currentNumber);
-            if (typeof previousNumber === 'number') {
-                currentNumber = '';
-            }
+            currentNumber = '';
         });
     });
 };
 
-function updateDisplay(currentNumber) {
-   
-    numbDispaly.textContent = currentNumber;
-}
+function getEquals() {
+
+    if(currentNumber !== '') {
+        currentNumber = parseFloat(currentNumber);
+        operate(currentNumber, operator, previousNumber)
+    }
+    currentNumber = value;
+};
+
+function updateDisplay(value) {
+    
+    numbDispaly.textContent = value;
+};
+
+function operate(currentNumber, operator, previousNumber) {
+    switch(operator) {
+        case '+':
+            value = previousNumber + currentNumber;
+            break;
+        case '-':
+            value = previousNumber - currentNumber;
+            break;
+        case '*':
+            value = previousNumber * currentNumber;
+            break;
+        case '/':
+            value = previousNumber / currentNumber;
+            break;
+        default:
+            value = null;
+    };
+
+console.log(value)
+updateDisplay(value)
+    
+};
 
 getCalcBtns();
 getOperatorsBtns();
