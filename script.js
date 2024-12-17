531d7d227c77ad8cc9b86ff4355972817a033c77
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-12-17 08:48:33 +0200

    added some instructions for myself fixed value dispaly problems

--- script.js
+++ script.js
@@ -5,27 +5,22 @@ let value = null;
 let operator = null;
 
 function getCalcBtns() {
-const operantsBtns = document.querySelectorAll('#operants button');
-
-operantsBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-      currentNumber += button.id
-      numbDisplay.textContent = currentNumber;
-
+    const operantsBtns = document.querySelectorAll('#operants button');
+    operantsBtns.forEach((button) => {
+        button.addEventListener('click', () => {
+            currentNumber += button.id
+            numbDisplay.textContent = currentNumber;    
+        });
     });
-});
-
 };
 
 function getOperatorsBtns(){
-const operatrosBtns = document.querySelectorAll('#operators button');
-
+    const operatrosBtns = document.querySelectorAll('#operators button');
     operatrosBtns.forEach((button) => {
         button.addEventListener('click', () => {
             operator = button.id;
             previousNumber = parseFloat(currentNumber);
             currentNumber = '';
-            
         });
     });
 };
@@ -36,15 +31,12 @@ function getEquals() {
         if(currentNumber !== '') {
             currentNumber = parseFloat(currentNumber);
             operate(currentNumber, operator, previousNumber);
-            
         };
-     });
-   
-    
+    });
 };
 
 function updateDisplay(value) {
-    currentNumber = Math.round(value * 100) / 100;
+    numbDisplay.textContent = Math.round(value * 100) / 100;
     currentNumber = value;
     console.log(value)
 };
@@ -62,18 +54,23 @@ function operate(currentNumber, operator, previousNumber) {
             break;
         case '/':
             if(currentNumber === 0) {
-                numbDisplay.textContent = 'ERROR';
-                return alert('Dividing by zero is not allowed!');
-            }
+                return numbDisplay.textContent = 'ERROR';
+                }
             value = previousNumber / currentNumber;
             break;
         default:
             value = null;
-        }
+    }
 
-    updateDisplay(value);
+updateDisplay(value);
 };
 
 getCalcBtns();
 getOperatorsBtns();
 getEquals();
+
+//Need to find a workoround if user presses multiple times operator. 
+//Find a way how do to that if user clicks number after first calcultion that numbers dont add but value will reset instead.
+//Find a way how to implement . button and that works by only once it is clicked.
+//Add reset button functionality.
+//Add backspace functionality.
